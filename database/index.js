ba890efecce1b3f351610affbdfeca431865f3f5
Ricardo <rroque98@gmail.com>
2018-07-13 13:53:30 -0700

    Completed repoSchema and save function in database.

--- database/index.js
+++ database/index.js
@@ -3,14 +3,26 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  full_name: String,
+  description: String,
+  url: String,
+  forks: Number,
+  watchers: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (data) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  var repoInstance = new Repo(data);
+
+  repoInstance.save(function(err) {
+    if (err) {
+      console.log(err);
+    }
+  });
 }
 
 module.exports.save = save;

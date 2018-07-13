232aaaffe2ec306b5c87bc53f13130490fc1f2de
Ricardo <rroque98@gmail.com>
2018-07-13 15:26:41 -0700

    initiated ajax post request

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -16,6 +16,29 @@ class App extends React.Component {
   search (term) {
     console.log(`${term} was searched`);
     // TODO
+    $.ajax({
+      method: "POST",
+      url: "http://localhost:1128/repos",
+      contentType: "application/json",
+      data: term,
+      success: function(data) {
+        console.log('search successful');
+        // initiate GET request
+        $.ajax({
+          method: 'GET',
+          url: "something...",
+          success: function(data) {
+            console.log('GET REQUEST successful');
+          },
+          error: function(err) {
+            console.log('unsuccessful GET request');
+          }
+        })
+      },
+      error: function(err) {
+        console.log('unsuccessful search');
+      }
+    });
   }
 
   render () {

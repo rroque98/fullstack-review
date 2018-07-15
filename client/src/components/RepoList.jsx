389a206befa8c71f3310049c5699591c67080d47
Ricardo <rroque98@gmail.com>
2018-07-14 23:56:42 -0700

    renders repos in react component

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -4,6 +4,23 @@ const RepoList = (props) => (
   <div>
     <h4> Repo List Component </h4>
     There are {props.repos.length} repos.
+    {props.repos.map(repo => (
+      <div key={repo.id}>
+        <div>
+          {repo.full_name}
+        </div>
+        <div>
+          {repo.url}
+        </div>
+        <div>
+          {repo.description}
+        </div>
+        <div>
+          {repo.forks}
+        </div>
+        {repo.watchers}
+      </div>
+    ))}
   </div>
 )
 
